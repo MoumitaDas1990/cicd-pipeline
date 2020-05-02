7621d20df319615ff4b41c0eb0001f128a8e5822
Moumita Das <moumitadas0991@gmail.com>
2020-05-02 23:03:49 +0530

    add init
    add init for sprint

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,13 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-  private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-  public static void main(String[] args) {
-    log.info("Main method has been invoked");
-    SpringApplication.run(Application.class, args);
-  }
-}
+public class Application extends SpringBootServletInitializer {}
